87ef228bf6a65b43d184e0111e2425bf115657ee
tom-aglow <tom@ohhhh.me>
2017-11-26 09:21:13 -0800

    add boiler plate for client app // add webpack and babel

---
+++ _client/src/ClientApp.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => {
+  return <div>
+		<h1>test</h1>
+	</div>;
+};
+
+render(<App />, document.getElementById('app'));
---
+++ _client/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './src/ClientApp.jsx',
+  devtool: 'cheap-eval-source-map',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    publicPath: '/public/'
+  }
+};
