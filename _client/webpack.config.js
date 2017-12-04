721cdc3b35b3d02115d749d02d733aff1cae3d1a
tom-aglow <tom@ohhhh.me>
2017-12-03 19:09:55 -0800

    add settings for webpack production build

--- _client/webpack.config.js
+++ _client/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
+const config = {
   context: __dirname,
   entry: [
     'react-hot-loader/patch',
@@ -66,3 +66,11 @@ module.exports = {
     historyApiFallback: true
   }
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.entry = './src/ClientApp.jsx';
+  config.devtool = false;
+  config.plugins = [];
+}
+
+module.exports = config;
