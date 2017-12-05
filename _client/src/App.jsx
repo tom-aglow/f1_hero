8b363d95ee6128370a56f66ad64cac860a34455f
tom-aglow <tom@ohhhh.me>
2017-12-04 19:51:31 -0800

    bring eslint into the project

--- _client/src/App.jsx
+++ _client/src/App.jsx
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, compose, createStore } from 'redux';
 import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 
-import Leaderboard from "./components/leaderboard/_Leaderboard";
-import Picks from "./components/picks/_Picks";
+import Leaderboard from './components/leaderboard/_Leaderboard';
+import Picks from './components/picks/_Picks';
 import Header from './components/Header';
 import Rules from './components/rules/_Rules';
 
 //	404 component placeholder
 const FourOhFour = () => <h1>404</h1>;
-
 
 //	app state store
 const store = createStore(
@@ -33,24 +32,20 @@ if (module.hot) {
   });
 }
 
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <div className="app">
-          <Header user={this.props.user} />
-          <Switch>
-            <Route exact path="/" component={Picks} />
-            <Route exact path="/picks" component={Picks} />
-            <Route path="/leaderboard" component={Leaderboard} />
-            <Route path="/rules" component={Rules} />
-            <Route component={FourOhFour} />
-          </Switch>
-        </div>
-      </Provider>
-    );
-  }
-}
+const App = props => (
+  <Provider store={store}>
+    <div className="app">
+      <Header user={props.user} />
+      <Switch>
+        <Route exact path="/" component={Picks} />
+        <Route exact path="/picks" component={Picks} />
+        <Route path="/leaderboard" component={Leaderboard} />
+        <Route path="/rules" component={Rules} />
+        <Route component={FourOhFour} />
+      </Switch>
+    </div>
+  </Provider>
+);
 
 export default App;
 export { store };
