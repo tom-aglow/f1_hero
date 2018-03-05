b320fde2c1b4c8c61d689c27e017275acf5819a3
tom-aglow <tom@ohhhh.me>
2018-03-05 12:09:26 -0800

    refactor /api/picks routes

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const racesController = require('../controllers/races');
 const driversController = require('../controllers/drivers');
+const picksController = require('../controllers/picks');
 const isAuth = require('../middlewares/isAuth');
 
 module.exports = () => {
@@ -9,6 +10,8 @@ module.exports = () => {
 
 	router.get('/races', isAuth, racesController.index);
 	router.get('/drivers', isAuth, driversController.index);
+	router.get('/picks/:round', isAuth, picksController.show);
+	router.post('/picks/:round', isAuth, picksController.store);
 
 	return router;
 };
