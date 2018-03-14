8f2bb8bfa3884e223f9127c00c080cc87e5b77a7
tom-aglow <tom@ohhhh.me>
2018-03-14 16:14:18 -0700

    tweak redux devtools setup

--- _client/src/store.js
+++ _client/src/store.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import { applyMiddleware, compose, combineReducers, createStore } from 'redux';
 import reduxThunk from 'redux-thunk';
 import dataReducer from './data/reducer';
@@ -6,12 +7,12 @@ const appReducer = combineReducers({
 	data: dataReducer
 });
 
-const devTools = () =>
-	typeof window === 'object' && typeof window.devToolsExtension !== 'undefined'
-		? window.devToolsExtension()
-		: f => f;
+const composeEnhancers =
+	typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		: compose;
 
-const enhancer = compose(applyMiddleware(reduxThunk), devTools());
+const enhancer = composeEnhancers(applyMiddleware(reduxThunk));
 
 const store = createStore(appReducer, enhancer);
 
