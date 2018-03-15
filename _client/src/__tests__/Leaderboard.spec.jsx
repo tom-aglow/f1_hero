54e3966e19590a768e35dc16a86d53d3b1104d4c
tom-aglow <tom@ohhhh.me>
2018-03-14 21:44:34 -0700

    delete unnecessary files

--- _client/src/__tests__/Leaderboard.spec.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { Unwrapped as Leaderboard } from '../components/leaderboard/_Leaderboard';
-import toJson from 'enzyme-to-json';
-
-test('Leaderboard renders correctly', async () => {
-	const component = shallow(<Leaderboard />);
-	expect(toJson(component)).toMatchSnapshot();
-});
--- _client/src/__tests__/Login.spec.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import toJson from 'enzyme-to-json';
-import Login from '../components/Login';
-import { shallow } from 'enzyme';
-
-test('Login renders correctly', () => {
-	const component = shallow(<Login />);
-	expect(toJson(component)).toMatchSnapshot();
-});
