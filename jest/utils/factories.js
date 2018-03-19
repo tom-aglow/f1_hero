1c7992deb73504ef6ccd2d938daa133840efd502
tom-aglow <tom@ohhhh.me>
2018-03-19 15:19:57 -0700

    refactor test utils modules

--- jest/utils/factories.js
+++ jest/utils/factories.js
@@ -4,74 +4,74 @@ const faker = require('faker');
 
 const { capitalizeFirstLetter } = require('../../utils/functions');
 
-const factory = (function testFactory() {
-	const self = {
-		createRace,
-		createUser,
-		createDriver,
-		createPick
-	};
+const self = {
+	createRace,
+	createUser,
+	createDriver,
+	createPick
+};
 
-	const publicApi = {
-		create
-	};
-
-	function create(model, fields = {}) {
-		const fn = self[`create${capitalizeFirstLetter(model)}`];
-		if (typeof fn === 'function') {
-			return fn(fields);
-		}
-		return null;
+function create(model, fields = {}) {
+	const fn = self[`create${capitalizeFirstLetter(model)}`];
+	if (typeof fn === 'function') {
+		return fn(fields);
 	}
+	throw new FactoryException(model);
+}
 
-	function createRace(fields) {
-		const Race = mongoose.model('race');
+function createRace(fields) {
+	const Race = mongoose.model('race');
 
-		return Race.create({
-			round: faker.random.number(20),
-			raceName: faker.random.words(3),
-			country: faker.address.country(),
-			date: faker.date.future(),
-			alpha3code: `_${faker.address.countryCode()}`,
-			flagUrl: faker.image.imageUrl(),
-			...fields
-		});
-	}
+	return Race.create({
+		round: faker.random.number(20),
+		raceName: faker.random.words(3),
+		country: faker.address.country(),
+		date: faker.date.future(),
+		alpha3code: `_${faker.address.countryCode()}`,
+		flagUrl: faker.image.imageUrl(),
+		...fields
+	});
+}
 
-	function createUser(fields) {
-		const User = mongoose.model('user');
+function createUser(fields) {
+	const User = mongoose.model('user');
 
-		return User.create({
-			twitterID: faker.random.uuid(),
-			username: faker.internet.userName(),
-			...fields
-		});
-	}
+	return User.create({
+		twitterID: faker.random.uuid(),
+		username: faker.internet.userName(),
+		...fields
+	});
+}
 
-	function createDriver(fields) {
-		const Driver = mongoose.model('driver');
-		const name = `${faker.name.firstName()} ${faker.name.lastName()}`;
+function createDriver(fields) {
+	const Driver = mongoose.model('driver');
+	const name = `${faker.name.firstName()} ${faker.name.lastName()}`;
 
-		return Driver.create({
-			number: faker.random.number(99),
-			name,
-			code: name.slice(0, 3).toUpperCase(),
-			...fields
-		});
-	}
+	return Driver.create({
+		number: faker.random.number(99),
+		name,
+		code: name.slice(0, 3).toUpperCase(),
+		...fields
+	});
+}
 
-	async function createPick(fields) {
-		const Pick = mongoose.model('pick');
+async function createPick(fields) {
+	const Pick = mongoose.model('pick');
 
-		return Pick.create({
-			_user: await self.createUser(),
-			_race: await self.createRace(),
-			forecast: [],
-			...fields
-		});
-	}
+	return Pick.create({
+		_user: await self.createUser(),
+		_race: await self.createRace(),
+		forecast: [],
+		...fields
+	});
+}
 
-	return publicApi;
-})();
+function FactoryException(model) {
+	this.message = `A factory wasn't registered for the model "${model}"`;
+}
+
+const factory = {
+	create
+};
 
 module.exports = factory;
--- jest/utils/renderer.js
+++ jest/utils/renderer.js
@@ -2,36 +2,32 @@
 const React = require('react');
 const { mount, shallow } = require('enzyme');
 
-const renderer = (function testRenderer() {
-	const publicApi = {
-		init,
-		render
+let component;
+let defaultProps;
+
+function init(_component, _defaultProps) {
+	component = _component;
+	defaultProps = _defaultProps;
+}
+
+function render(props = {}, isShallow = false) {
+	const propsToUse = {
+		...defaultProps,
+		...props
 	};
 
-	let component;
-	let defaultProps;
-
-	function init(_component, _defaultProps) {
-		component = _component;
-		defaultProps = _defaultProps;
+	if (isShallow) {
+		return shallow(React.createElement(component, propsToUse), {
+			disableLifecycleMethods: true
+		});
 	}
 
-	function render(props = {}, isShallow = false) {
-		const propsToUse = {
-			...defaultProps,
-			...props
-		};
+	return mount(React.createElement(component, propsToUse));
+}
 
-		if (isShallow) {
-			return shallow(React.createElement(component, propsToUse), {
-				disableLifecycleMethods: true
-			});
-		}
-
-		return mount(React.createElement(component, propsToUse));
-	}
-
-	return publicApi;
-})();
+const renderer = {
+	init,
+	render
+};
 
 module.exports = renderer;
